acdd4b16181bda2e46ff326ff437913d143b9ba2
Sinancvk0 <sinancivak00@gmail.com>
2023-06-08 21:46:44 +0300

    DbContext ve DbSet Oluşturuldu

---
+++ ReceteX/ReceteX.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceteX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceteX.Data
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+
+		}
+		public virtual DbSet<AppUser>
+
+	}
+}
