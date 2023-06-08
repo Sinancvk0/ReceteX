b3b96f7f43b39c307eaa80f3a849a813d4518d84
Sinancvk0 <sinancivak00@gmail.com>
2023-06-08 21:47:01 +0300

    .

--- ReceteX/ReceteX.Data/ApplicationDbContext.cs
+++ ReceteX/ReceteX.Data/ApplicationDbContext.cs
@@ -8,14 +8,33 @@ using System.Threading.Tasks;
 
 namespace ReceteX.Data
 {
-	public class ApplicationDbContext:DbContext
+	public class ApplicationDbContext : DbContext
 	{
-		public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
 
 
 		}
-		public virtual DbSet<AppUser>
+		public virtual DbSet<AppUser> Users { get; set; }
+		public virtual DbSet<Customer> Customer { get; set; }
+
+		public virtual DbSet<Description> Descriptions { get; set; }
+		public virtual DbSet<DescriptionType> DescriptionTypes { get; set; }
+
+		public virtual DbSet<Diagnosis> Diagnoses { get; set; }
+
+		public virtual DbSet<Medicine> Medicines { get; set; }
+
+		public virtual DbSet<MedicineUsagePeriod> MedicineUsagePeriods { get; set; }
+
+		public virtual DbSet<MedicineUsageType> MedicineUsageTypes { get; set; }
+		public virtual DbSet<Prescription> Prescriptions { get; set; }
+
+		public virtual DbSet<PrescriptionsMedicine> PrescriptionsMedicines { get; set; }
+
+		public virtual DbSet<Status> Statuses { get; set; }
+
+
 
 	}
 }
