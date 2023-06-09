88d27daa8bb22fe5eebd1e6ca76771a441a5fb0c
Sinancvk0 <sinancivak00@gmail.com>
2023-06-09 17:20:13 +0300

    Web in içindeki model silindi ve view içindeki  error  silindi home controller düzenlendi

--- ReceteX/ReceteX.Web/Controllers/HomeController.cs
+++ ReceteX/ReceteX.Web/Controllers/HomeController.cs
@@ -1,18 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ReceteX.Web.Models;
+
 using System.Diagnostics;
 
 namespace ReceteX.Web.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
-
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
-
 		public IActionResult Index()
 		{
 			return View();
@@ -23,10 +16,6 @@ namespace ReceteX.Web.Controllers
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+		
 	}
 }
--- ReceteX/ReceteX.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReceteX.Web.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
