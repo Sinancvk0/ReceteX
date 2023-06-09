7fa65707315f481ab5a4291c77f575044b1927a4
Sinancvk0 <sinancivak00@gmail.com>
2023-06-09 18:13:14 +0300

    login

--- ReceteX/ReceteX.Web/Controllers/UserController.cs
+++ ReceteX/ReceteX.Web/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using ReceteX.Models;
 using ReceteX.Repository.Shared.Abstract;
+using System.Security.Claims;
 
 namespace ReceteX.Web.Controllers
 {
@@ -16,6 +20,49 @@ namespace ReceteX.Web.Controllers
 
 		}
 		[HttpPost]
+
+		public async Task<IActionResult> login (AppUser user)
+		{
+			if (user != null)
+			{
+				AppUser usr = unitOfWork.Users.GetFirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+
+				if (usr != null)
+				{
+					List<Claim> claims = new List<Claim>();
+					claims.Add(new Claim(ClaimTypes.Name, usr.Name));
+					claims.Add(new Claim(ClaimTypes.NameIdentifier, usr.Id.ToString()));
+					if (usr.isAdmin)
+					{
+						claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+					}
+					else
+					{
+						claims.Add(new Claim(ClaimTypes.Role, "User"));
+					}
+					var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+					await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+					await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+					return RedirectToAction("Index", "Home");
+
+				}
+				else
+				{
+					return View();
+				}
+			
+
+			}
+			else
+			{
+				return View();
+			}
+
+
+		}
 
 	}
 }
