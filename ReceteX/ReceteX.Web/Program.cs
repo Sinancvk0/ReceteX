06d5ed6f3a588c5e51fb3fcd8c37643c20677b52
Sinancvk0 <sinancivak00@gmail.com>
2023-06-09 16:16:36 +0300

    connection strings eklendi ve nugetlar webe eklendi Repository klasörler oluşturuldu

--- ReceteX/ReceteX.Web/Program.cs
+++ ReceteX/ReceteX.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ReceteX.Data;
+
 namespace ReceteX.Web
 {
 	public class Program
@@ -8,6 +11,9 @@ namespace ReceteX.Web
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
+
+			builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("connstr")));	
+
 
 			var app = builder.Build();
 
