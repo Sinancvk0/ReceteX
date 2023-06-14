5d55967ba33558050f78738af41159beda105194
Sinancvk0 <sinancivak00@gmail.com>
2023-06-14 18:14:09 +0300

    Medicine Controller ve Index Oluşturuldu

--- ReceteX/ReceteX.Web/Controllers/MedicineController.cs
+++ ReceteX/ReceteX.Web/Controllers/MedicineController.cs
@@ -1,9 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReceteX.Models;
+using ReceteX.Repository.Shared.Abstract;
+using System.Xml;
 
 namespace ReceteX.Web.Controllers
 {
 	public class MedicineController : Controller
 	{
+		private readonly IUnitOfWork unitOfWork;
+
+		public MedicineController(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork;
+		}
+
+		public async Task ParseAndSaveFromXml(string xmlContent)
+		{
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml(xmlContent);
+			XmlNodeList medicines = xmlDoc.SelectNodes("ilaclar/ilac");
+			
+			foreach (XmlNode medicine in medicines )
+			{
+				Medicine med = new Medicine();
+				med.Name = medicine.SelectSingleNode("ad").InnerText;
+				med.Barcode = medicine.SelectSingleNode("barkod").InnerText;
+
+				unitOfWork.Medicines.Add(med);
+			}
+			unitOfWork.Save();
+
+		}
+
 		public IActionResult Index()
 		{
 			return View();
