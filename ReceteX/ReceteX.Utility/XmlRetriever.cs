9b9ea4f092398129a18d25773ae2c4f3c9cc739f
Sinancvk0 <sinancivak00@gmail.com>
2023-06-14 17:11:19 +0300

    XmlRetriever   genel bir metot yazıldı

---
+++ ReceteX/ReceteX.Utility/XmlRetriever.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceteX.Utility
+{
+	public class XmlRetriever
+	{
+		public async Task<string> GetXmlContent(string url)
+		{
+			using (var client = new HttpClient())
+			{
+				
+				HttpResponseMessage response = await client.GetAsync(url);
+				response.EnsureSuccessStatusCode();
+
+				return await response.Content.ReadAsStringAsync();
+			}
+		}
+
+	}
+
+}
+
